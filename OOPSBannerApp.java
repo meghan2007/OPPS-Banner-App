41ffe09e86d3da448223809d88d346a2bb70c047
meghan2007 <meghan4167@gmail.com>
2026-02-17 22:29:16 +0530

    UC7: Refactor banner using InnerClass

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,49 +1,88 @@
+
 public class OOPSBannerApp {
 
-    public static String getOPattern() {
-        return String.join("\n",
+    
+    public static class CharacterPatternMap {
+
+        private char character;
+        private String[] pattern;
+
+        
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        
+        public char getCharacter() {
+            return character;
+        }
+
+        
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    
+    public static String[] getOPattern() {
+        return new String[]{
                 " *** ",
                 "*   *",
                 "*   *",
                 "*   *",
                 "*   *",
                 "*   *",
-                " *** ");
+                " *** "
+        };
     }
 
-    public static String getPPattern() {
-        return String.join("\n",
+    
+    public static String[] getPPattern() {
+        return new String[]{
                 "**** ",
                 "*   *",
                 "*   *",
                 "**** ",
                 "*    ",
                 "*    ",
-                "*    ");
+                "*    "
+        };
     }
 
-    public static String getSPattern() {
-        return String.join("\n",
+    
+    public static String[] getSPattern() {
+        return new String[]{
                 " ****",
                 "*    ",
                 "*    ",
                 " *** ",
                 "    *",
                 "    *",
-                "**** ");
+                "**** "
+        };
     }
 
+    
     public static void main(String[] args) {
 
-        String[] banner = {
-                getOPattern(),
-                getOPattern(),
-                getPPattern(),
-                getSPattern()
+        CharacterPatternMap[] letters = {
+                new CharacterPatternMap('O', getOPattern()),
+                new CharacterPatternMap('O', getOPattern()),
+                new CharacterPatternMap('P', getPPattern()),
+                new CharacterPatternMap('S', getSPattern())
         };
 
-        for (String letter : banner) {
-            System.out.println(letter);
+        // Print row by row using StringBuilder
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (CharacterPatternMap letter : letters) {
+                line.append(letter.getPattern()[row]).append("  ");
+            }
+
+            System.out.println(line);
         }
     }
 }
